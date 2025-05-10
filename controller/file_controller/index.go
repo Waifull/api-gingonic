7581c61a1188fcd4d5babe5351098fb1cb224623
Wafiul Achdi <achdiwafiul@gmail.com>
2025-05-10 15:20:35 +0700

    edit file controller validation

--- controller/file_controller/index.go
+++ controller/file_controller/index.go
@@ -1,10 +1,8 @@
 package file_controller
 
 import (
-	"fmt"
 	"gin-gonic-gorm/utils"
 	"path/filepath"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,18 +17,31 @@ func HandlerUploadFile(ctx *gin.Context){
 		return
 	}
 
+	//validation by file extension
+	fileExtension := []string{".jpg", ".jpg", ".jpeg", ".pdf"}
+	isFileValidated := utils.FileValidationByExtension(fileHeader, fileExtension)
+	if !isFileValidated {
+		ctx.AbortWithStatusJSON(400, gin.H{
+			"message": "file not allowed.",
+		})
+		return
+	}
+
+	//validation by file content-type
+	// fileType := []string{"image/jpg"}
+	// isFileValidated := utils.FileValidation(fileHeader, fileType)
+	// if !isFileValidated {
+	// 	ctx.AbortWithStatusJSON(400, gin.H{
+	// 		"message": "file not allowed.",
+	// 	})
+	// 	return
+	// }
 
 	extensionFile := filepath.Ext(fileHeader.Filename)
-	loc, err := time.LoadLocation("Asia/Jakarta")
-	if err != nil {
-		
-		panic(err)
-	}
-	currentTime := time.Now().In(loc).Format("20060102")
-	filename := fmt.Sprintf("%s-%s%s",currentTime, utils.RandomString(5), extensionFile)
+	filename := utils.RandomFileName(extensionFile)
 
-	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", filename))
-	if errUpload != nil {
+	isSaved := utils.SaveFile(ctx, fileHeader, filename)
+	if !isSaved {
 		ctx.JSON(500, gin.H{
 			"message": "internal server error, can't save file.",
 		})
--- utils/file.go
+++ utils/file.go
@@ -1,12 +1,86 @@
 package utils
 
-import "math/rand"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"mime/multipart"
+	"path/filepath"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 func RandomString(n int) string {
 
+	rand.Seed(time.Now().UnixMilli())
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(b)
+}
+
+func FileValidation(fileHeader *multipart.FileHeader, fileType []string) bool {
+
+	contentType := fileHeader.Header.Get("Content-Type")
+	log.Println("content-type", contentType)
+	result := false
+
+	for _, typeFile := range fileType {
+		
+		if contentType == typeFile {
+			result = true
+			break
+		}
+	}
+
+	return result
+}
+
+func FileValidationByExtension(fileHeader *multipart.FileHeader, fileExtension []string) bool {
+
+	extension := filepath.Ext(fileHeader.Filename)
+	log.Println("extension", extension)
+	result := false
+
+	for _, typeFile := range fileExtension {
+		
+		if extension == typeFile {
+			result = true
+			break
+		}
+	}
+
+	return result
+}
+
+func RandomFileName(extensionFile string, prefix ...string) string {
+	currentPrefix := "file"
+	if len(prefix) > 0 {
+		if prefix[0] != "" {
+			currentPrefix = prefix[0]
+		}
+	}
+
+
+	loc, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		
+		panic(err)
+	}
+	currentTime := time.Now().In(loc).Format("20060102")
+	filename := fmt.Sprintf("%s-%s-%s%s", currentPrefix, currentTime, RandomString(5), extensionFile)
+
+	return filename
+}
+
+func SaveFile(ctx *gin.Context, fileHeader *multipart.FileHeader, filename string) bool {
+	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", filename))
+	if errUpload != nil {
+		log.Println("Can't save file.")
+		return false
+	} else {
+		return true
+	}
 }
