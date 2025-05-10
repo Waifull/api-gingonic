f6c95bb632eebe7aa6cf40ab0c98b4fdce9d0d57
Wafiul Achdi <achdiwafiul@gmail.com>
2025-05-10 13:19:29 +0700

    Update filename format to match Indonesian date (WIB) in YYYYMMDD

--- controller/file_controller/index.go
+++ controller/file_controller/index.go
@@ -2,6 +2,9 @@ package file_controller
 
 import (
 	"fmt"
+	"gin-gonic-gorm/utils"
+	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,15 +19,17 @@ func HandlerUploadFile(ctx *gin.Context){
 		return
 	}
 
-	// file, errFile := fileHeader.Open()
-	// if errFile != nil {
-	// 	panic(errFile)
-	// }
 
-	// extensionFile := filepath.Ext(fileHeader.Filename)
-	// filename := ""
+	extensionFile := filepath.Ext(fileHeader.Filename)
+	loc, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		
+		panic(err)
+	}
+	currentTime := time.Now().In(loc).Format("20060102")
+	filename := fmt.Sprintf("%s-%s%s",currentTime, utils.RandomString(5), extensionFile)
 
-	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", fileHeader.Filename))
+	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", filename))
 	if errUpload != nil {
 		ctx.JSON(500, gin.H{
 			"message": "internal server error, can't save file.",
